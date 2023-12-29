68bb3c3eca2defb0d526f373b23e2084430bee06
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2023-12-29 16:00:01 -0300

    first commit

---
+++ grafbase.config.ts
@@ -0,0 +1,29 @@
+import { graph, config } from '@grafbase/sdk'
+
+
+const g = graph.Standalone()
+
+const User = g.model('User', {
+  name: g.string().length({ min: 3, max: 50 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional()
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3, max: 50 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
+})
+
+
+export default config({
+  graph: g
+})
